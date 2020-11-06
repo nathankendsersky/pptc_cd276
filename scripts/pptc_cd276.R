e7ab2ef5e6797e89cb24d79971b7685aa28d9ea0
nathankendsersky <nate.kendsersky@gmail.com>
2020-11-06 08:31:18 -0500

    Through figure 1b.

--- scripts/pptc_cd276.R
+++ scripts/pptc_cd276.R
@@ -8,6 +8,9 @@ setwd("./")
 
 #### figure 1 ####
 
+# figure 1a
+# Treehouse Tumors vs. GTEx, CD276 TPM
+
 tree.gtex.tpm <- read.delim("data/Treehouse-GTEx-CD276-tpm.txt",header = T,sep = "\t")
 histo.colors <- read.delim("data/Histology-Colors.txt",header=T,sep = "\t")
 
@@ -34,4 +37,36 @@ fig1a <- ggplot(tree.gtex.tpm.color, aes(x=Histology,y=CD276_tpm,fill=Histology)
         legend.position = "none") + 
   ylab("CD276 TPM")
 fig1a
+
+
+# figure 1b
+# PPTC tumors, CD276 TPM
+
+pptc.tpm <- read.delim("data/PPTC-CD276-tpm.txt",header = T,sep = "\t")
+histo.colors <- read.delim("data/Histology-Colors.txt",header=T,sep = "\t")
+
+# tree.gtex.tpm$Histology <- factor(tree.gtex.tpm$Histology, levels = c("Ewing sarcoma","Rhabdoid tumor","RMS","Embryonal RMS","Alveolar RMS","Hepatoblastoma",
+#                                                                       "Meningioma","Neuroblastoma","Osteosarcoma","Wilms tumor",
+#                                                                       "Adipose Tissue","Adrenal Gland",
+#                                                                       "Blood","Blood Vessel","Bone Marrow","Bladder","Brain","Breast",
+#                                                                       "Colon","Esophagus","Fallopian Tube","Heart","Kidney","Liver","Lung",
+#                                                                       "Muscle","Nerve","Ovary","Pancreas","Pituitary",
+#                                                                       "Prostate","Small Intestine","Salivary Gland","Skin","Stomach","Spleen",
+#                                                                       "Testis","Thyroid","Uterus","Cervix Uteri","Vagina"),order=T)
+
+histo.colors.plot1b <- histo.colors %>%
+  dplyr::filter(Histology %in% pptc.tpm$Histology) %>%
+  dplyr::pull(Color) %>%
+  as.character()
+
+fig1b <- ggplot(pptc.tpm, aes(x=Histology,y=CD276_tpm,fill=Histology)) + 
+  geom_boxplot(color="black",alpha=.9,outlier.size=0.5) + 
+  theme_bw() + 
+  scale_fill_manual(values = histo.colors.plot1b) +
+  #geom_jitter(shape=16,position = position_jitter(0.2),alpha=0.8) +
+  theme(axis.text.x = element_text(size = 8, angle = 45, hjust = 1,colour = "black"),
+        axis.title.y = element_text(size=8),axis.title.x=element_blank(), axis.text.y = element_text(size = 8,colour = "black"),
+        legend.position = "none") + 
+  ylab("CD276 TPM")
+fig1b
 
